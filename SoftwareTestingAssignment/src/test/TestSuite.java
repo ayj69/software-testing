dd7013450c990703ed98cf34223b4fec38938ac9
ayj69 <86646606+ayj69@users.noreply.github.com>
2022-04-15 07:53:17 +0800

    Add files via upload

--- SoftwareTestingAssignment/src/test/TestSuite.java
+++ SoftwareTestingAssignment/src/test/TestSuite.java
@@ -2,16 +2,15 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-
 @RunWith(value = Suite.class)
-@SuiteClasses(value = {ClientTest.class,
-TestDeliveryItemList.class,
-TestDocument.class,
-TestEmail.class,
-TestItem.class,
-TestParcel.class,
-testUserList.class
-})
-public class TestSuite {
+@SuiteClasses(value = { ClientTest.class, 
+						TestDeliveryItemList.class, 
+						TestDocument.class, 
+						TestEmail.class,
+						TestItem.class,
+						TestParcel.class, 
+						testUserList.class })
 
+
+public class TestSuite {
 }
