b20026e376b828fad844b7d9b782fecd21a31818
ayj69 <86646606+ayj69@users.noreply.github.com>
2022-04-15 12:53:36 +0800

    Add files via upload

---
+++ SoftwareTestingAssignment/src/main/DeliveryStaff.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class DeliveryStaff extends User {
+
+	private String employeeID;
+
+	DeliveryStaff(String username, String password, String phoneNum) {
+		super(username, phoneNum, password);
+	}
+
+}
---
+++ SoftwareTestingAssignment/src/main/Guest.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Guest extends Client {
+
+	private double distance;
+
+	// constructor
+	Guest(String username, String phoneNum, double distance) {
+		super(username, phoneNum, "-", distance);
+		this.setDistance(distance);
+	}
+
+	public double getDistance() {
+		return distance;
+	}
+
+	public void setDistance(double distance) {
+		this.distance = distance;
+	}
+
+}
